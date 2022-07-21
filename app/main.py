2c6b82b450830efa04ac0addd20519f357de7f6e
Jian Shen <jianshen92@gmail.com>
2022-07-22 01:47:41 +0800

    Install FastAPI

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+def hello_world():
+    return {'Hello': 'World'}
--- src/hello.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-
-import datetime
-
-if __name__ == '__main__':
-    print(f'{datetime.datetime.now().isoformat()}: Today is a good day')
